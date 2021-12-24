f83f7b9b4686ab731a10c063f77e25a3702c79b4
passiveradio <srccreator@gmail.com>
2021-12-24 20:56:05 +0900

    [update] updated run.py

--- run.py
+++ run.py
@@ -2,12 +2,11 @@ from fft import sequense_fft, fft
 
 
 if __name__ == "__main__":
+    
     base_path = "spectrum/"
-    endpoint = "data_42_005.csv"
     output_path = "fft_results/"
     
     # txt_to_csv("spectrum_txt/", output_path)
-    
     # print( pd.read_csv(base_path+endpoint).columns)
     
     sequense_fft(base_path, output_dir=output_path, header=7)
